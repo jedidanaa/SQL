a255a77e1f7eff536065c9e038a8fd99bbd71439
jedidanaa <jedidanaashidaaoduanortey@gmail.com>
2026-05-10 18:36:55 +0000

    Add files via upload

---
+++ Guessthedatabase.py
@@ -0,0 +1,8 @@
+print("What is the type of the above given database - \n Relational database \n 2)Non-Realational Database")
+answer = int(input("Enter your guess here....."))
+if answer == 2:
+    print("You guessed it right!")
+else:
+    print("Unfortnately, your guess was wrong")
+
+print("\n Please tell your mentor why you guessed this?")
